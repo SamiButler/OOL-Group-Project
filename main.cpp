beb2a64b91f48e570ac6f6877f4e34890142a782
Sami <samibutler1994@gmail.com>
2021-10-12 13:53:03 +1030

    Adding main file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "THIS IS WHERE THE INTO MESSAGE WILL GO" << endl;
+
+	cout << "Choose from the following characters:" << endl;
+
+	cout << endl << "1 Scott Pilgrim" << endl << "2 Tony Montana" << endl << "3 Skinny Pete" << endl << "4 Dirty Harry" << endl << "5 Chuck Norris" << endl;	
+
+
+
+	return 0;
+}
