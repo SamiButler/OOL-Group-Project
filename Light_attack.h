a0dd0ac6ee3b08f255c6f9590294a11e9337ad87
Joe-Harvey <joe44harvey@gmail.com>
2021-10-15 15:02:22 +1030

    Light_attack updates

---
+++ Light_attack.h
@@ -0,0 +1,13 @@
+#ifndef LIGHT_ATTACK.H
+#define LIGHT_ATTACK.H
+
+#include "Move.h"
+
+class Light_attack{
+    public:
+    Light_attack();
+    change_opponent_health();
+    change_user_health();
+};
+#endif
+
