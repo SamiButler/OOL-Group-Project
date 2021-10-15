6ae2581a5a21505fd951539d7bea90f0bbb9f041
Sami <samibutler1994@gmail.com>
2021-10-15 13:54:19 +1030

    Update

--- opponent_array.cpp
+++ opponent_array.cpp
@@ -4,10 +4,29 @@
 
 using namespace std;
 
-string* opponent_array(Character user, int* random_array)
+Character* opponent_array(Character user, int* random_array)
 // This function will return an array of Character names in random order.
 {	
-	string* opponents = new string[4];
+	Character* opponents = new Character[4];
+	string all_names[5] = {"Scott Pilgrim", "Tony Montana", "Skinny Pete", "Dirty Harry", "Chuck Norris"};
+	string name_array[4];
+	int count;
+
+	if (user.name == "Scott Pilgrim")
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			if (all_names[i] == "Scott Pilgrim")
+			{
+				count++;
+			}
+			name_array[i] = all_names[count];
+			count++;
+		}
+	}
 
 	return opponents;
 }
+
+
+
