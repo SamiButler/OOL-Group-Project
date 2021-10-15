d2fb3cc433288fc6e8fb430d4edc3c7db0c877f3
jakerutter-UoA <a1827369@student.adelaide.edu.au>
2021-10-15 11:23:22 +1030

    move.cpp file, as well as some comments on the .h file

--- Move.h
+++ Move.h
@@ -8,7 +8,9 @@ class Move
 public:
 	Move();          		 // a type parameter will be used to determine the move's recharge times as 
 	int heavy_recharge_time; //well as how the move reacts to its opponent (eg if type is 3, 
-	int special_recharge_time;
+	int special_recharge_time; //depending on how we choose to proceed
+	                           //a subclass for every move type or use a variable to define each type
+	
 	
 	~Move();
 };
