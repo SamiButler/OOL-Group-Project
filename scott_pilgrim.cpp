6954b48715dd6a7512a992da79962278e2e3dd16
Joe-Harvey <joe44harvey@gmail.com>
2021-10-21 22:04:44 +1030

    update

--- scott_pilgrim.cpp
+++ scott_pilgrim.cpp
@@ -5,14 +5,6 @@
 
 using namespace std;
 
-scott_pilgrim::scott_pilgrim(string character_name, int character_number)
-{
-	name = character_name;
-	ID = character_number;
-	hitpoints = 100;
-	heavy_recharge_time = 0;
-	special_recharge_time = 1;
-}
 
 int scott_pilgrim::special_attack()
 {
