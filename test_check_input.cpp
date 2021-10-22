8c5335c2f25f05ce35be1ed7555024056eef6100
Joe-Harvey <joe44harvey@gmail.com>
2021-10-22 12:44:00 +1030

    Update

---
+++ test_check_input.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+extern bool check_input(string input, int numOpts);
+
+int main(){
+	cout << "string Input: 5" << endl << endl;
+	for (int i = 1; i <= 10; i++){
+		cout << "numOpts Input: " << i;
+		if (i > 5){
+			cout << "Expected Output: false";
+		}
+		else if (i <=5){
+			cout << "Expected Output: true";
+		}
+		cout << "    Output: " <<  check_input("5", i) << endl
+	}
+	return 0;
+}
